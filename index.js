953c9fc23073cb52b530b8d4ba6d580288823648
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-07 08:00:18 +0600

    find users single data from mongodb server system added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
 //used for .env file active or use.
@@ -32,6 +32,14 @@ async function run() {
       const query = {};
       const cursor = serviceClloection.find(query);
       const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    //get user's single data from mongoDB server
+    app.get("/services/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: id };
+      const result = await serviceClloection.findOne(query);
       res.send(result);
     });
   } finally {
