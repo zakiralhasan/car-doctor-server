df0fab2137e4f1e91944536f13477e54b0d888e3
zakiralhasan <zakiralhasan.bd@gmail.com>
2022-11-12 15:39:14 +0600

    jwt system added

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 5000;
 
+//used for jwt authentication
+const jwt = require("jsonwebtoken");
+
 //used for .env file active or use.
 require("dotenv").config();
 
@@ -23,6 +26,23 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+
+//function for JWT
+function verifyJwt(req, res, next) {
+  const jwtHeader = req.headers.authorization;
+  if (!jwtHeader) {
+    res.status(401).send({ message: "unauthorized user" });
+  }
+  const token = jwtHeader.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (error, dcode) {
+    if (error) {
+      res.status(401).send({ message: "unauthorized user", error });
+    }
+    req.dcode = dcode;
+    next();
+  });
+}
+
 async function run() {
   try {
     const serviceClloection = client.db("carDoctor").collection("services");
@@ -52,7 +72,13 @@ async function run() {
     });
 
     //get user's order data from mongoDB server
-    app.get("/orders", async (req, res) => {
+    app.get("/orders", verifyJwt, async (req, res) => {
+      //JWT functon used here
+      const dcode = req.dcode.email;
+      console.log("Inside email query API", dcode);
+      if (dcode !== req.query.email) {
+        return res.status(403).send({ message: "unauthorized user" });
+      }
       let query = {};
       if (req.query.email) {
         query = { email: req.query.email };
@@ -73,13 +99,22 @@ async function run() {
     //update single data at mongodb server
     app.patch("/orders/:id", async (req, res) => {
       const id = req.params.id;
-      // const status = req.body.status;
+      const status = req.body.status;
       const query = { _id: ObjectId(id) };
       const updatedData = {
         $set: { status: "Approved" },
       };
       const result = await orderCollcetion.updateOne(query, updatedData);
       res.send(result);
+    });
+
+    //jwt authentication
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+        expiresIn: "1h",
+      });
+      res.send({ token });
     });
   } finally {
   }
